cbb8b2ebc101ba541b45a0954c3cca695fef873b
Petrus Savolainen <petrus.savolainen@gmail.com>
2022-03-03 10:40:23 +0200

    Changed queries.py functions and imported only config function from config.py

--- src/data/queries.py
+++ src/data/queries.py
@@ -1,14 +1,16 @@
 import psycopg2
-import config
+from config import config
 
 # select_all(cursor)tällä voi kutsua connectia
+
+
 
 def connect():
     con = None
     try:
-        con = psycopg2.connect(**config.config())
+        con = psycopg2.connect(**config())
         cursor = con.cursor()
-        select_certs(cursor)
+        insert(cursor)
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -17,8 +19,8 @@ def connect():
             con.close()
             
             
-def select_all(cursor):
-    SQL = 'SELECT * FROM certificates'
+def select(SQL, cursor):
+    SQL 
     cursor.execute(SQL)
     colnames = [desc[0] for desc in cursor.description]
     print(colnames)
@@ -27,25 +29,18 @@ def select_all(cursor):
         print(row)
         row = cursor.fetchone()
         
-def select_columns(cursor):
-    SQL = "SELECT Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'certificate'"
-    cursor.execute(SQL)
-    colnames = [desc[0] for desc in cursor.description]
-    print(colnames)
-    row = cursor.fetchone()
-    while row is not None:
-        print(row)
-        row = cursor.fetchone()
         
-def select_certs(cursor):
-    SQL = "SELECT person.name, person.id, certificates.name FROM certificates, person WHERE certificates.person_id = person.id ;"
-    cursor.execute(SQL)
+def insert(cursor):
+    SQL = "INSERT INTO certificates (name, person_id) VALUES (%s, %s);"
+    data = ("Azure", "2")
+    cursor.execute(SQL, data)
     colnames = [desc[0] for desc in cursor.description]
     print(colnames)
     row = cursor.fetchone()
     while row is not None:
         print(row)
         row = cursor.fetchone()
+
         
 if __name__ == '__main__':
     connect()
