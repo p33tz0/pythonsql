f913132c256c9d182a6b6f4c6bd74ad3b258ca73
Petrus Savolainen <petrus.savolainen@gmail.com>
2022-03-03 09:29:09 +0200

    First commit, database.ini, config.py for access to DB, queries.py

---
+++ src/data/queries.py
@@ -0,0 +1,16 @@
+
+def connect():
+    con = None
+    try:
+        con = psycopg2.connect(**config())
+        cursor = con.cursor()
+        SQL = 'SELECT * FROM person;'
+        cursor.execute(SQL)
+        row = cursor.fetchone()
+        print(row)
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
